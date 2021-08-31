64f54531b438adb091a8cbef1b2f455c5a4c8215
Ganikota Sreedhar <gandikotasreedhar98@gmail.com>
2021-08-31 13:55:15 +0530

    add food card

--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -1,18 +1,64 @@
 import React from "react";
+import { AiOutlineCompass } from "react-icons/ai";
+import { BiTimeFive } from "react-icons/bi";
 
 //components
 import MenuListContainer from "../../Components/Restaurant/Order-Online/MenuListContainer";
 import FloatMenuBtn from "../../Components/Restaurant/Order-Online/FloatMenuBtn";
+import FoodItem from "../../Components/Restaurant/Order-Online/FoodItem";
 
 const OrderOnline = () => {
   return (
     <>
-      <div className="w-full">
+      <div className="w-full flex">
         <aside className="hidden md:flex flex-col gap-3 border-r overflow-y-scroll border-gray-200 h-screen w-1/4">
           <MenuListContainer />
           <MenuListContainer />
         </aside>
-        <div className="w-full md:3/4"></div>
+        <div className="w-full md:3/4 flex flex-col gap-3">
+          <div className="px-3">
+            <h2 className="text-xl font-semibold">Order online</h2>
+            <h4 className="flex gap-3 items-center font-light text-gray-500">
+              <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
+            </h4>
+          </div>
+          <section>
+            <FoodItem
+              image="https://b.zmtcdn.com/data/dish_photos/d23/b61bd95f61310d91b811cbc407f8fd23.jpg?fit=around|130:130&crop=130:130;*,*"
+              title="Kheema Masala"
+              rating="4"
+              price="₹1000"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+            />
+          </section>
+          <section>
+            <FoodItem
+              image="https://b.zmtcdn.com/data/dish_photos/d23/b61bd95f61310d91b811cbc407f8fd23.jpg?fit=around|130:130&crop=130:130;*,*"
+              title="Kheema Masala"
+              rating="4"
+              price="₹1000"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+            />
+          </section>
+          <section>
+            <FoodItem
+              image="https://b.zmtcdn.com/data/dish_photos/d23/b61bd95f61310d91b811cbc407f8fd23.jpg?fit=around|130:130&crop=130:130;*,*"
+              title="Kheema Masala"
+              rating="4"
+              price="₹1000"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+            />
+          </section>
+          <section>
+            <FoodItem
+              image="https://b.zmtcdn.com/data/dish_photos/d23/b61bd95f61310d91b811cbc407f8fd23.jpg?fit=around|130:130&crop=130:130;*,*"
+              title="Kheema Masala"
+              rating="4"
+              price="₹1000"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+            />
+          </section>
+        </div>
       </div>
       <FloatMenuBtn />
     </>
